1e66996e02f85f0a796aca5869227e7e83fd1d8f
pvthuyet <pvthuyet@gmail.com>
2020-11-02 21:09:53 +0700

    update FileWatcher.cpp

--- FileWatcher/FileWatcher.cpp
+++ FileWatcher/FileWatcher.cpp
@@ -11,13 +11,22 @@
 
 import FiboLogger;
 import FiboMinidump;
+import FiboStringUtils;
 
 int wmain(int argc, wchar_t* argv[])
 {
+    using namespace std::string_literals;
+    using namespace std::string_view_literals;
     fibo::MiniDump::monitoring();
     try
     {
         fibo::Logger::createLogger();
+        SPDLOG_INFO("{}", fibo::StringUtils::convert(L"hello world from wc2mb"));
+        SPDLOG_INFO(L"{}", fibo::StringUtils::convert("hello world from mb2wc"));
+        SPDLOG_INFO(fibo::StringUtils::randAlphabet(32));
+
+        auto vt = fibo::StringUtils::split("hello,world,ok"sv, ","sv);
+        for(const auto& e : vt) SPDLOG_INFO(e);
     }
     catch (std::exception const& e)
     {
