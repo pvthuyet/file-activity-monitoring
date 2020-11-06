163b979444f4c11444931a31814c7e1cfed235b0
pvthuyet <pvthuyet@gmail.com>
2020-11-06 15:41:24 +0700

    fix template for string => done

--- FileWatcher/FileWatcher.cpp
+++ FileWatcher/FileWatcher.cpp
@@ -15,63 +15,45 @@ import FiboMinidump;
 import FiboStringUtils;
 import FiboConcept;
 
-#include <iostream>
-#include <type_traits>
-
-template <typename T, typename U>
-struct decay_equiv :
-    std::is_same<std::remove_cvref_t<std::remove_pointer_t<std::decay_t<T>>>, U>::type
-{};
-
-void testDecay()
-{
-    std::cout << std::boolalpha
-        << decay_equiv<char, char>::value << '\n'
-        << decay_equiv<char&, char>::value << '\n'
-        << decay_equiv<char&&, char>::value << '\n'
-        << decay_equiv<const char&, char>::value << '\n'
-        << decay_equiv<char[100], char>::value << '\n'
-        << decay_equiv<char*, char>::value << '\n'
-        << decay_equiv<const char*, char>::value << '\n'
-        ;
-}
-
-
-template<typename SRC, typename TOKEN> requires fibo::StringablePair<SRC, TOKEN>
-auto split2(const SRC& s, const TOKEN& token)
-{
-    //using demoType = fibo::TString_t<std::string>;
-    std::cout << "is_string_v: " << fibo::is_string_v<SRC> << '\n';
-    using typ = fibo::TString_t<SRC>;
-    std::cout << typeid(typ).name() << '\n';
-}
-
 int test()
 {
-    split2(std::string("a"), std::string{"b"});
+    static_assert(std::is_same_v<char,      fibo::primitive_string_t<char[12]>>, "Not the same type");
+    static_assert(std::is_same_v<char,      fibo::primitive_string_t<char const[12]>>, "Not the same type");
+    static_assert(std::is_same_v<char,      fibo::primitive_string_t<char const [12]>>, "Not the same type");
+    static_assert(std::is_same_v<char,      fibo::primitive_string_t<char *>>, "Not the same type");
+    static_assert(std::is_same_v<char,      fibo::primitive_string_t<char const *>>, "Not the same type");
+    static_assert(std::is_same_v<char,      fibo::primitive_string_t<char * const>>, "Not the same type");
+    static_assert(std::is_same_v<char,      fibo::primitive_string_t<std::string>>, "Not the same type");
+    static_assert(std::is_same_v<char,      fibo::primitive_string_t<std::string_view>>, "Not the same type");
+    static_assert(std::is_same_v<wchar_t,   fibo::primitive_string_t<std::wstring>>, "Not the same type");
+    static_assert(std::is_same_v<wchar_t,   fibo::primitive_string_t<std::wstring_view>>, "Not the same type");
+    static_assert(std::is_same_v<char8_t,   fibo::primitive_string_t<std::u8string>>, "Not the same type");
+    static_assert(std::is_same_v<char8_t,   fibo::primitive_string_t<std::u8string_view>>, "Not the same type");
+    static_assert(std::is_same_v<char16_t,  fibo::primitive_string_t<std::u16string>>, "Not the same type");
+    static_assert(std::is_same_v<char16_t,  fibo::primitive_string_t<std::u16string_view>>, "Not the same type");
+    static_assert(std::is_same_v<char32_t,  fibo::primitive_string_t<std::u32string>>, "Not the same type");
+    static_assert(std::is_same_v<char32_t,  fibo::primitive_string_t<std::u32string_view>>, "Not the same type");
     return 0;
 }
 
 int wmain(int argc, wchar_t* argv[])
 {
-    return test();
-
     using namespace std::string_literals;
     using namespace std::string_view_literals;
     fibo::MiniDump::monitoring();
     try
     {
         fibo::Logger::createLogger();
-        //SPDLOG_INFO("{}", fibo::StringUtils::convert(L"hello world from wc2mb"));
-        //SPDLOG_INFO(fibo::StringUtils::randAlphabet(32));
+        SPDLOG_INFO("{}", fibo::StringUtils::convert(L"hello world from wc2mb"));
+        SPDLOG_INFO(fibo::StringUtils::randAlphabet(32));
 
         // split
-        //auto vt = fibo::StringUtils::split(std::string{ "hello,world,ok" }, std::string{ "," });
-        //for(const auto& e : vt) SPDLOG_INFO(e);
+        auto vt = fibo::StringUtils::split(std::string{ "hello,world,ok" }, std::string{ "," });
+        for(const auto& e : vt) SPDLOG_INFO(e);
 
         // equal
-        //auto eq = fibo::StringUtils::equal("hello", "helLo"sv, true);
-        //SPDLOG_INFO("equal: {}", eq);
+        auto eq = fibo::StringUtils::equal("hello"s, "helLo", true);
+        SPDLOG_INFO("equal: {}", eq);
     }
     catch (std::exception const& e)
     {
